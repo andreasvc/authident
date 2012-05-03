105400388c5c1a6da5e7e31d2b328cee8f7dfc61
Andreas van Cranenburgh <andreas@unstable.nl>
2012-05-04 01:39:03 +0200

    fixed fragment finding script

--- README.md
+++ README.md
@@ -15,6 +15,6 @@ Procedure:
 - prepare a directory "books/" with one directory for each author, each containing one file per work in UTF-8.
 - edit parseworks.sh to set the right paths and run. Will parse documents with stanford parser
 - run devtestsplits.py: make dev-test-train set splits with leave-one-out cross-validation, results written to "splits/"
-- edit commonfragments.sh to set the location of disco-dop and run. Will extract syntactic fragments, results written to "out/"
+- edit commonfragments.sh to set the location of disco-dop and run. Will extract syntactic fragments, results written to "fragments/"
 - run evaluate.py: do classification & evaluation
 
--- commonfragments.sh
+++ commonfragments.sh
@@ -1,24 +1,43 @@
 #!/bin/bash
 NUMPROC=16
+OUTDIR=fragments
+DEVORTEST=test
 FRAGMENTSEEKER=../disco-dop/fragmentseeker.py
-mkdir -p out
-N=0
-TOTAL=$[ `ls -1 splits/*/*.train | wc -l` * `ls -1 splits/*/*.dev* | wc -l` ]
 
-for authortext in splits/*.train
+mkdir -p $OUTDIR
+N=0
+# 4 folds, 5 authors against 5 authors, test each work 5 times
+TOTAL=$[ 4 * 5 * 5 * 5 ]
+
+for part in `seq 0 4`
 do
-	for testtext in splits/*.dev*
+	for fold in `seq 0 3`
 	do
-		output=out/"$testauthor"_$authortext
-		(python $FRAGMENTSEEKER --numproc 1 --quiet \
-			"$authortext" "$testtext" > $output \
-		&& wc -l $output \
-		|| echo error with $output) &
-		echo compare $authortext with $testtext write to out/"$testauthor"_$authortext
-		N=$[ $N + 1 ]
-		if [[ $[$N % $NUMPROC] -eq 0 ]]; then
-			echo waiting for batch $[ $N / $NUMPROC ] of $[ $TOTAL / $NUMPROC ]
-			wait
-		fi
+		for testtext in splits/*.$fold.$DEVORTEST$part
+		do
+			aa=`basename "$testtext"`
+			for authortext in splits/*.train$fold
+			do
+				bb=`basename "$authortext"`
+				output=$OUTDIR/"$bb"_"$aa"
+				# if file does not exist or has zero-size:
+				if [ ! -e "$output" ] || [ ! -s "$output" ]; then
+					#echo compare $authortext with $testtext write to $output
+					#echo python $FRAGMENTSEEKER --exact --numproc 1 --quiet "$authortext" "$testtext" \> "$output"
+					(python $FRAGMENTSEEKER \
+						--exact --numproc 1 --quiet \
+						"$authortext" "$testtext" > "$output" \
+						&& (wc -l "$output" && echo Job $N of $TOTAL finished) \
+						|| echo error with $output) &
+					N=$[ $N + 1 ]
+					if [[ $[$N % $NUMPROC] -eq 0 ]]; then
+						echo -n waiting for batch $[ $N / $NUMPROC ] 
+						echo " of $[ $TOTAL / $NUMPROC ]"
+						wait
+					fi
+				fi
+			done
+		done
 	done
 done
+echo $N / $TOTAL
