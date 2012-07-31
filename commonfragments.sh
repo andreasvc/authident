87a8e27881b367344b8d930e4a171c14f341d2a0
Andreas van Cranenburgh <andreas@unstable.nl>
2012-07-31 21:24:28 +0200

    reflect rename of fragmentseeker.py -> fragments.py

--- commonfragments.sh
+++ commonfragments.sh
@@ -3,7 +3,7 @@ NUMPROC=16
 INDIR=$1
 OUTDIR=$2
 DEVORTEST=$3
-FRAGMENTSEEKER=../disco-dop/fragmentseeker.py
+FRAGMENTSEEKER=../disco-dop/fragments.py
 
 echo extracting fragments
 for fold in `seq 0 3`; do
@@ -11,7 +11,7 @@ for fold in `seq 0 3`; do
 		if [ -e "$authortext" ]; then
 			(find $INDIR -name "*.$fold.$DEVORTEST*" -print0 \
 			| xargs --null \
-				python $FRAGMENTSEEKER --exact --quiet --batch $OUTDIR "$authortext" \
+				python $FRAGMENTSEEKER --quiet --batch $OUTDIR "$authortext" \
 			&& echo "Compared all unknown test chunks in fold $fold to known texts of $authortext " \
 			|| echo error with $authortext) &
 		fi
